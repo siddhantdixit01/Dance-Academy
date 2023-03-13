b309c8d2fd2eb60190212b050177926b56b732ba
Siddhant Dixit <siddhantdixit97@gmail.com>
2023-03-13 21:51:22 +0530

    13/03/2022

--- danceCategories.php
+++ danceCategories.php
@@ -9,10 +9,10 @@
 ?>
 <?php include('inc/header.php');?>
     <div class="container">
-        <div class="col=md-3">
+        <div class="col-md-3">
             <?php include('sidebar/adminSidebar.php');?>
         </div>
-        <div class="col=md-9" style="">
+        <div class="col-md-9" style="">
             <div class="row" style="margin-left:1px;margin-top:18px;">
                 <a href="" class="btn btn-info" data-toggle="modal" data-target="#exampleModal">
                     Add Category
@@ -30,9 +30,52 @@
                 </thead>
                 <tbody>
                     <?php $sql="SELECT * FROM tbl_dance_categories";
-                        $getCategories=mysqli_query($conn,$sql); ?>
+                        $getCategories=mysqli_query($conn,$sql); 
+                        if(mysqli_num_rows($getCategories)>0){
+                            while ($row=mysqli_fetch_assoc($getCategories)) {
+                    ?>
+                                <tr>
+                                    <td style="width:5%;font-size:14px;">
+                                        <?php echo $row['category_id'];?>
+                                    </td>
+                                    <td style="width:35%;font-size:14px;">
+                                        <?php echo $row['category_name'];?>
+                                    </td>
+                                    <td style="width:5%;font-size:14px;">
+                                        <?php echo $row['tag_name'];?>
+                                    </td>
+                                    <td style="width:5%;font-size:14px;" class="image">
+                                        <img src="<?php echo $row['category_image'];?>" alt="Dance" 
+                                        style="width:30%;">
+                                    </td>
+                                    <td style="width:25%;" class="actions">
+                                        <a href="" class="btn-sm btn-primary editCategory" 
+                                        data-val=<?php echo $row['category_id'];?> data-toggle="modal" data-target="#editCategory">
+                                            <i class="fa fa-edit"></i>Edit
+                                        </a>
+                                        <a href="" class="btn-sm btn-danger editCategory" 
+                                        data-val=<?php echo $row['category_id'];?>>
+                                            <i class="fa fa-trash"></i>Delete
+                                        </a>
+                                    </td>
+                                </tr>
+                    <?php            
+                            }
+                        }   
+                        else {
+                    ?>
+                            <tr>
+                                <td>No categories added yet!</td>
+                            </tr>
+                    <?php        
+                        } 
+                    ?>
                 </tbody>
             </table>
+        </div>
+        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labledby="exampleModalLabel"
+        aria-hidden="true">
+            
         </div>
     </div>
 <?php include('inc/footer.php');?>
